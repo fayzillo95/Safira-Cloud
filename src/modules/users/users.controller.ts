72afa10f709a6c270d2714c108ed78c8cf2b06d0
Fayzillo Ummatov <ovovovlululutvata@gmail.com>
2025-07-16 02:31:59 +0800

    user boldi (#8)
    Co-authored-by: Omadbek Tuxtasinboyev <omadbek1223erubdnx@gmail.com>

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -1,34 +1,59 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiTags, ApiOperation, ApiResponse, ApiParam, ApiBody } from '@nestjs/swagger';
 
+@ApiTags('Users') // Swagger bo'lim nomi
 @Controller('users')
 export class UsersController {
-  constructor(private readonly usersService: UsersService) {}
+  constructor(private readonly usersService: UsersService) { }
 
   @Post('create-user')
+  @ApiOperation({ summary: 'Yangi foydalanuvchi yaratish' })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({ status: 201, description: 'Foydalanuvchi yaratildi' })
   create(@Body() createUserDto: CreateUserDto) {
     return this.usersService.create(createUserDto);
   }
 
-  @Get()  
+  @Get('getAll')
+  @ApiOperation({ summary: 'Barcha foydalanuvchilarni olish' })
+  @ApiResponse({ status: 200, description: 'Barcha foydalanuvchilar ro‘yxati' })
   findAll() {
     return this.usersService.findAll();
   }
 
-  @Get(':id')
+  @Get('getOne/:id')
+  @ApiOperation({ summary: 'Bitta foydalanuvchini olish' })
+  @ApiParam({ name: 'id', description: 'Foydalanuvchi ID raqami' })
+  @ApiResponse({ status: 200, description: 'Topilgan foydalanuvchi' })
   findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+    return this.usersService.findOne(id);
   }
 
-  @Patch(':id')
+  @Patch('updateOne/:id')
+  @ApiOperation({ summary: 'Foydalanuvchini yangilash' })
+  @ApiParam({ name: 'id', description: 'Foydalanuvchi ID raqami' })
+  @ApiBody({ type: UpdateUserDto })
+  @ApiResponse({ status: 200, description: 'Foydalanuvchi yangilandi' })
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+    return this.usersService.update(id, updateUserDto);
   }
 
-  @Delete(':id')
+  @Delete('delete/:id')
+  @ApiOperation({ summary: 'Foydalanuvchini o‘chirish' })
+  @ApiParam({ name: 'id', description: 'Foydalanuvchi ID raqami' })
+  @ApiResponse({ status: 200, description: 'Foydalanuvchi o‘chirildi' })
   remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+    return this.usersService.remove(id);
   }
 }
