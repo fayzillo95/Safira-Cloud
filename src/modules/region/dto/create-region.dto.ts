d55a465eba9c50034775a9d075a77e4439f66b13
DaminboevM <m701rizo@gmail.com>
2025-07-15 21:55:43 +0500

    first commit

--- src/modules/region/dto/create-region.dto.ts
+++ src/modules/region/dto/create-region.dto.ts
@@ -1,1 +1,10 @@
-export class CreateRegionDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsString, MinLength } from 'class-validator';
+
+export class CreateRegionDto {
+
+  @ApiProperty({ example: 'Toshkent', description: 'Region nomi minimal uzunligi 2-ta belgi !' })
+  @IsString()
+  @MinLength(3)
+  name: string;
+}
--- src/modules/region/region.controller.ts
+++ src/modules/region/region.controller.ts
@@ -1,34 +1,48 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Param, Patch, Delete, ParseIntPipe } from '@nestjs/common';
 import { RegionService } from './region.service';
 import { CreateRegionDto } from './dto/create-region.dto';
 import { UpdateRegionDto } from './dto/update-region.dto';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
-@Controller('region')
+
+@ApiTags('Regions')
+@Controller('regions')
 export class RegionController {
   constructor(private readonly regionService: RegionService) {}
 
-  @Post()
-  create(@Body() createRegionDto: CreateRegionDto) {
-    return this.regionService.create(createRegionDto);
-  }
-
   @Get()
+  @ApiOperation({ summary: 'Barcha regionlarni olish' })
   findAll() {
     return this.regionService.findAll();
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.regionService.findOne(+id);
+  @ApiOperation({ summary: 'ID boyicha regionni olish' })
+  @ApiParam({ name: 'id', type: Number, description: 'Region ID' })
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.regionService.findOne(id);
+  }
+
+  @Post()
+  @ApiOperation({ summary: 'Yangi region yaratish', description: 'minimal 3-ta belgidan iborat bolgan region nomini kiritish kk !' })
+  create(@Body() payload: CreateRegionDto) {
+    return this.regionService.create(payload);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateRegionDto: UpdateRegionDto) {
-    return this.regionService.update(+id, updateRegionDto);
+  @ApiOperation({ summary: 'Regionni yangilash', description: 'minimal 3-ta belgidan iborat bolgan region nomini kiritish kk !' })
+  @ApiParam({ name: 'id', type: Number, description: 'Region ID' })
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() payload: UpdateRegionDto,
+  ) {
+    return this.regionService.update(id, payload);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.regionService.remove(+id);
+  @ApiOperation({ summary: 'Regionni ochirish' })
+  @ApiParam({ name: 'id', type: Number, description: 'Region ID' })
+  remove(@Param('id', ParseIntPipe) id: number) {
+    return this.regionService.remove(id);
   }
 }
