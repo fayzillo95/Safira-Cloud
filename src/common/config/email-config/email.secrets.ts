af14fa47ee9792fc89b81b3bf11d207e62b12e7b
fayzillo95 <ovovovlululutvata@gmail.com>
2025-07-15 22:49:59 +0500

    user.service.ts   bcrypt.hash  salt value assign parseInt

--- src/common/config/email-config/email.secrets.ts
+++ src/common/config/email-config/email.secrets.ts
@@ -1,9 +1,9 @@
-export const emailModuleOptions ={
-      host: 'smtp.ethereal.email',
-      port: 587,
-      secure: false, // TLS emas
-      auth: {
+export const emailModuleOptions = {
+    host: 'smtp.ethereal.email',
+    port: 587,
+    secure: false, // TLS emas
+    auth: {
         user: 'josue77@ethereal.email',
         pass: 'ufJFzuAW5kVn7geCq6',
-      },
-    }
+    },
+}
