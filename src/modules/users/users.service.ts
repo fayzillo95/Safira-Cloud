890ef7b2e10e059815edf83378d643f132d8b0e5
Omadbek Tuxtasinboyev <omadbek1223erubdnx@gmail.com>
2025-07-15 23:30:38 +0500

    user boldi

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,9 +1,10 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from 'src/core/prisma/prisma.service';
 import * as bcrypt from "bcrypt"
 import { ConfigService } from '@nestjs/config';
+import { NotFoundError } from 'rxjs';
 
 @Injectable()
 export class UsersService {
@@ -13,7 +14,7 @@ export class UsersService {
     if (existUser) throw new ConflictException("Foydaluvchi allaqachon mavjud!")
 
     const round = this.configService.get<string>('BCRYPT_SALT_ROUNDS')
-    const hashPasword = await bcrypt.hash(createUserDto.password,round ? parseInt(round) : 10)
+    const hashPasword = await bcrypt.hash(createUserDto.password, round ? parseInt(round) : 10)
     const createUser = await this.prisma.user.create({
       data: {
         email: createUserDto.email,
@@ -44,12 +45,13 @@ export class UsersService {
         user: {
           id: createUser.id,
           name: createUser.name,
-          email: createUser.email,
           profile: {
+            id: createProfile.id,
             userId: createProfile.userId,
           },
           contact: {
             userId: createContact.userId,
+            email: createContact.email,
             phone: createContact.phone,
             instagram: createContact.instagram,
             telegram: createContact.telegram
@@ -64,19 +66,81 @@ export class UsersService {
     }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAll() {
+    const information = await this.prisma.user.findMany({
+      select: {
+        id: true,
+        name: true,
+        email: true,
+        regionId: true,
+        role: true,
+        profile: {
+          select: {
+            id: true,
+            avatar: true,
+            userId: true,
+            bio: true
+          }
+        }
+      }
+    })
+    return {
+      success: true,
+      data: information
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findOne(id: string) {
+    const existUser = await this.prisma.user.findUnique({ where: { id } })
+    if (!existUser) throw new NotFoundException("user not found!!")
+
+    const { password, ...safeUser } = existUser
+
+    return {
+      success: true,
+      data: safeUser
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
+  async update(id: string, updateUserDto: UpdateUserDto) {
+    const existUser = await this.prisma.user.findUnique({ where: { id } });
+    const isEmail = await this.prisma.user.findUnique({ where: { email: updateUserDto.email } })
+    if (isEmail) throw new ConflictException("bu email allaqachon mavjud")
+    if (!existUser) throw new NotFoundException("User not found!");
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+
+    let data: any = { ...updateUserDto };
+    if (updateUserDto.password) {
+      const round = this.configService.get<string>('BCRYPT_SALT_ROUNDS');
+      data.password = await bcrypt.hash(updateUserDto.password, round ? parseInt(round) : 10);
+    }
+
+    const updatedUser = await this.prisma.user.update({
+      where: { id },
+      data,
+    });
+
+    const { password, ...safeUser } = updatedUser;
+
+    return {
+      success: true,
+      message: "User muvaffaqiyatli yangilandi",
+      data: safeUser,
+    };
+  }
+  async findByEmail() {
+
+  }
+  async remove(id: string) {
+    const existUser = await this.prisma.user.findUnique({ where: { id } });
+
+    if (!existUser) throw new NotFoundException("User not found!");
+
+    await this.prisma.user.delete({ where: { id } });
+
+    return {
+      success: true,
+      message: `User #${id} muvvafaqiyatli uchirildi`
+    };
   }
 }
