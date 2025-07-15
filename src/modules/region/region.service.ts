60a083dfe4f88eb7bb6da29580a86ae44132f65a
Fayzillo Ummatov <ovovovlululutvata@gmail.com>
2025-07-16 01:29:18 +0800

    first commit (#4)
    Co-authored-by: DaminboevM <m701rizo@gmail.com>

--- src/modules/region/region.service.ts
+++ src/modules/region/region.service.ts
@@ -1,26 +1,43 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRegionDto } from './dto/create-region.dto';
 import { UpdateRegionDto } from './dto/update-region.dto';
+import { PrismaService } from 'src/core/prisma/prisma.service';
 
 @Injectable()
 export class RegionService {
-  create(createRegionDto: CreateRegionDto) {
-    return 'This action adds a new region';
+  constructor(private prisma: PrismaService) {}
+  
+  
+  async findAll() {
+    return this.prisma.region.findMany();
+  }
+  
+  
+  async findOne(id: number) {
+    return this.prisma.region.findUnique({ where: { id } });
+  }
+  
+  
+  async create(payload: CreateRegionDto) {
+    await this.prisma.region.create({ data: payload })
+    return {success: true, message: 'Region succes created !'}
   }
 
-  findAll() {
-    return `This action returns all region`;
+  
+  async update(id: number, payload: UpdateRegionDto) {
+    if(!await this.prisma.region.findUnique({where: {id}})) {
+      throw new NotFoundException({success: false, message: 'Region not found !'})
+    }
+    await this.prisma.region.update({ where: { id }, data: payload, })
+    return {success: true, message: 'Region succes created !'}
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} region`;
-  }
 
-  update(id: number, updateRegionDto: UpdateRegionDto) {
-    return `This action updates a #${id} region`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} region`;
+  async remove(id: number) {
+    if(!await this.prisma.region.findUnique({where: {id}})) {
+      throw new NotFoundException({success: false, message: 'Region not found !'})
+    }
+    return this.prisma.region.delete({ where: { id } });
   }
 }
+
