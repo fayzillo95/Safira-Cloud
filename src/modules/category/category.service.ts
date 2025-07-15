34e89d5e31a819c36f9b83d25843c2d455a98834
Ozodbek-cloud <vortextblade16@gmail.com>
2025-07-15 21:48:02 +0500

    Category System

--- src/modules/category/category.service.ts
+++ src/modules/category/category.service.ts
@@ -1,26 +1,59 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/create-category.dto';
 import { UpdateCategoryDto } from './dto/update-category.dto';
+import { PrismaService } from 'src/core/prisma/prisma.service';
 
 @Injectable()
 export class CategoryService {
-  create(createCategoryDto: CreateCategoryDto) {
-    return 'This action adds a new category';
+  constructor(private prismaService: PrismaService) { }
+
+  async create(createCategoryDto: CreateCategoryDto) {
+    let created_category = await this.prismaService.category.create({ data: createCategoryDto })
+    if (!created_category) throw new BadRequestException('Error Detected On Category,  Creating Time')
+
+    return { success: true, message: 'Successfully Created Category', data: created_category }
   }
 
-  findAll() {
-    return `This action returns all category`;
+  async findAll() {
+    let all_categories = await this.prismaService.category.findMany(
+      {
+        include: { services: true }
+      }
+    )
+    return { success: true, message: 'Successfully Get All Categories with Services', data: all_categories }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} category`;
+  async findOne(id: number) {
+    let found_category = await this.prismaService.category.findFirst(
+      {
+        where: { id: id }
+      }
+    )
+    if (!found_category) throw new NotFoundException(`This ${id} Is Not Found!`)
+
+    return { success: true, message: 'Successfully Getted One Category', data: found_category }
   }
 
-  update(id: number, updateCategoryDto: UpdateCategoryDto) {
-    return `This action updates a #${id} category`;
+  async update(id: number, updateCategoryDto: UpdateCategoryDto) {
+    let findAndUpdate = await this.prismaService.category.update(
+      {
+        where: { id: id }, data: updateCategoryDto
+      }
+    )
+    if (!findAndUpdate) throw new NotFoundException(`This ${id} Is Not Found`)
+
+    return { success: true, message: `Successfully Updated ${id} Category`, data: findAndUpdate }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} category`;
+  async remove(id: number) {
+    let deleted = await this.prismaService.category.delete(
+      {
+        where: { id: id }
+      }
+    )
+    if (!deleted) throw new NotFoundException(`This ${id} Is Not Found`)
+
+    return {success: true, message: `Successfully Deleted ${id} Category`, data: deleted.name}
+    
   }
 }
