e033049e40c7541bbf131e1f7f2b17f5d4d0a578
DaminboevM <m701rizo@gmail.com>
2025-07-15 23:37:07 +0500

    servicedagi hatolik tog'rilandi

--- src/modules/region/region.service.ts
+++ src/modules/region/region.service.ts
@@ -9,58 +9,47 @@ import { PrismaService } from 'src/core/prisma/prisma.service';
 
 @Injectable()
 export class RegionService {
-<<<<<<< HEAD
-  create(createRegionDto: CreateRegionDto) {
-    return 'This action adds a new region';
-  }
-
-  findAll() {
-    return `This action returns all region`;
-=======
   constructor(private prisma: PrismaService) {}
   
   
   async findAll() {
     return this.prisma.region.findMany();
   }
-  
-  
+
+
   async findOne(id: number) {
     return this.prisma.region.findUnique({ where: { id } });
   }
-  
-  
+
+
   async create(payload: CreateRegionDto) {
-    if(await this.prisma.region.findUnique({where: {name: payload.name}})) {
-      throw new ConflictException({success: false, message: 'Region alredy exists !'})
+    if (await this.prisma.region.findUnique({ where: { name: payload.name } })) {
+      throw new ConflictException({ success: false, message: 'Region alredy exists !' })
     }
     await this.prisma.region.create({ data: payload })
-    return {success: true, message: 'Region succes created !'}
+    return { success: true, message: 'Region succes created !' }
   }
 
-  
+
   async update(id: number, payload: UpdateRegionDto) {
-    if(!await this.prisma.region.findUnique({where: {id}})) {
-      throw new NotFoundException({success: false, message: 'Region not found !'})
+    if (!await this.prisma.region.findUnique({ where: { id } })) {
+      throw new NotFoundException({ success: false, message: 'Region not found !' })
     }
-    if(await this.prisma.region.findUnique({where: {name: payload.name}})) {
-      throw new ConflictException({success: false, message: 'Region alredy exists !'})
+    if (await this.prisma.region.findUnique({ where: { name: payload.name } })) {
+      throw new ConflictException({ success: false, message: 'Region name alredy exists !' })
     }
     await this.prisma.region.update({ where: { id }, data: payload, })
     return {success: true, message: 'Region succes created !'}
->>>>>>> 8df8998 (servicedagi hatolik tog'rilandi)
   }
 
   findOne(id: number) {
     return `This action returns a #${id} region`;
   }
 
-  update(id: number, updateRegionDto: UpdateRegionDto) {
-    return `This action updates a #${id} region`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} region`;
+  async remove(id: number) {
+    if(!await this.prisma.region.findUnique({where: {id}})) {
+      throw new NotFoundException({success: false, message: 'Region not found !'})
+    }
+    return this.prisma.region.delete({ where: { id } });
   }
 }
-
