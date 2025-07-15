25a5b9fe32446a8a28d8746b28c37b1ba12aad61
Fayzillo Ummatov <ovovovlululutvata@gmail.com>
2025-07-16 01:52:26 +0800

    Auth (#6)
    * auth service
    
    * user.service.ts   bcrypt.hash  salt value assign parseInt

--- src/common/config/email-config/email.secrets.ts
+++ src/common/config/email-config/email.secrets.ts
@@ -1,9 +1,36 @@
-export const emailModuleOptions ={
-      host: 'smtp.ethereal.email',
-      port: 587,
-      secure: false, // TLS emas
-      auth: {
-        user: 'josue77@ethereal.email',
-        pass: 'ufJFzuAW5kVn7geCq6',
-      },
-    }
+export const emailModuleOptions = {
+    host: 'smtp.gmail.com',
+    port: 465,
+    secure: true,
+    auth: {
+      user: 'fayzillofn30@gmail.com',
+      pass: 'hwhphlkacfqynuve',
+    },
+  }
+
+
+
+
+
+
+
+
+// {
+//     host: 'smtp.ethereal.email',
+//     port: 587,
+//     secure: false, // TLS emas
+//     auth: {
+//         user: 'josue77@ethereal.email',
+//         pass: 'ufJFzuAW5kVn7geCq6',
+//     },
+// }
+
+// {
+//     host: 'smtp.gmail.com',
+//     port: 465,
+//     secure: true,
+//     auth: {
+//       user: 'fayzillofn30@gmail.com',
+//       pass: 'hwhphlkacfqynuve',
+//     },
+//   }
