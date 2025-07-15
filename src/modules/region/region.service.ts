5f0ec4355a7977c262d37c2933c8d1fa71e64196
Fayzillo Ummatov <ovovovlululutvata@gmail.com>
2025-07-16 01:29:18 +0800

    first commit (#4)
    Co-authored-by: DaminboevM <m701rizo@gmail.com>

--- src/modules/region/region.service.ts
+++ src/modules/region/region.service.ts
@@ -1,26 +1,26 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { CreateRegionDto } from './dto/create-region.dto';
 import { UpdateRegionDto } from './dto/update-region.dto';
 import { PrismaService } from 'src/core/prisma/prisma.service';
 
 @Injectable()
 export class RegionService {
-  constructor(private prisma: PrismaService) {}
-  
-  
+  constructor(private prisma: PrismaService) { }
+
+
   async findAll() {
     return this.prisma.region.findMany();
   }
-  
-  
+
+
   async findOne(id: number) {
     return this.prisma.region.findUnique({ where: { id } });
   }
 
 
   async create(payload: CreateRegionDto) {
-    if(await this.prisma.region.findUnique({where: {name: payload.name}})) {
-      throw new ConflictException({success: false, message: 'Region alredy exists !'})
+    if (await this.prisma.region.findUnique({ where: { name: payload.name } })) {
+      throw new ConflictException({ success: false, message: 'Region alredy exists !' })
     }
     await this.prisma.region.create({ data: payload })
     return { success: true, message: 'Region succes created !' }
@@ -31,8 +31,8 @@ export class RegionService {
     if (!await this.prisma.region.findUnique({ where: { id } })) {
       throw new NotFoundException({ success: false, message: 'Region not found !' })
     }
-    if(await this.prisma.region.findUnique({where: {name: payload.name}})) {
-      throw new ConflictException({success: false, message: 'Region alredy exists !'})
+    if (await this.prisma.region.findUnique({ where: { name: payload.name } })) {
+      throw new ConflictException({ success: false, message: 'Region name alredy exists !' })
     }
     await this.prisma.region.update({ where: { id }, data: payload, })
     return { success: true, message: 'Region succes created !' }
@@ -46,3 +46,4 @@ export class RegionService {
     return this.prisma.region.delete({ where: { id } });
   }
 }
+
